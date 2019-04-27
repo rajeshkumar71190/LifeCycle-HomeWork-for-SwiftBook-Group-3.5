bd8d5688756da69b3c4c3aafaf5fc6ca90e4e0fd
MichaelTseitlin <49906602+MichaelTseitlin@users.noreply.github.com>
2019-04-27 13:26:03 +0300

    Added methods and labels

--- LifeCycle/ViewController.swift
+++ LifeCycle/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var arrayOfMethods = [String]()
+
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var yellowLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        printMessage()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        printMessage()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        printMessage()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        printMessage()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        printMessage()
+    }
+    
+    func printMessage(line: Int = #line, function: String = #function) {
+        print("\(title ?? "nil"). Line: \(line) of function \(function)")
+        arrayOfMethods.append(function)
+        let string = arrayOfMethods.joined(separator: "\n")
+        greenLabel.text = "\(title ?? "nil") \(string)"
+    
+    }
 
 }
 

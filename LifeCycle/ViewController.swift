a7ef61b40265efb047dfabc1c04f82f09f1e5998
MichaelTseitlin <49906602+MichaelTseitlin@users.noreply.github.com>
2019-04-27 12:04:02 +0300

    Initial Commit

---
+++ LifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by Michael Tseitlin on 4/27/19.
+//  Copyright © 2019 Michael Tseitlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
